4db70692a7e70042c29cac2b3e4ab364214342b0
Chris Johnson <chris@crisistextline.org>
2014-03-12 10:24:24 -0400

    Comment the SHIT outta this thang.

--- js/app/Campaign.js
+++ js/app/Campaign.js
@@ -1,17 +1,32 @@
-var Campaign = function (data) {
-  var defaults = {
-    title: 'New Campaign', // label
-    description: 'Take your dad to get his blood pressure checked', // teaser
-    url: '#', // url
 
-    staffPick: false, // bs_field_staff_pick
+/**
+ * Campaign JS object representation
+ * @param {Object} data The raw campaign data
+ */
+var Campaign = function (data) {
+  // Default values for a newly created campaign
+  var defaults = {
+    title: 'New Campaign',
+    description: 'Take your dad to get his blood pressure checked',
+    url: '#',
+    staffPick: false,
     featured: false,
-    image: 'http://lorempixel.com/600/600?' + Math.random() //
+    image: 'http://lorempixel.com/600/600?' + Math.random()
   };
 
+  // Merge the campaign object from data with the defaults into
+  // this campaign object
   $.extend(this, defaults, this.convert(data));
 };
 
+/**
+ * Convert fields from the Solr results to relevant local, friendlier
+ * field names.
+ *
+ * @param  {Object} data The raw campaign object
+ *
+ * @return {Object} The new data translated and ignoring
+ */
 Campaign.prototype.convert = function (data) {
   var newData = {};
   var map = {
@@ -20,9 +35,10 @@ Campaign.prototype.convert = function (data) {
     'url': 'url',
     'bs_field_staff_pick': 'staffPick',
     'bs_sticky': 'featured',
-    //'ss_field_search_image': 'image'
+    'ss_field_search_image': 'image'
   };
 
+  // Only populate fields that we care about from the map.
   for (var i in data) {
     if (map[i] !== undefined) {
       newData[map[i]] = data[i];
@@ -32,19 +48,27 @@ Campaign.prototype.convert = function (data) {
   return newData;
 };
 
+/**
+ * Render a campaign
+ * @return {jQuery} The jQuery object that is the rendered representation of this Campaign
+ */
 Campaign.prototype.render = function () {
+  // Clone from the embedded template
   var $wrapper = $('#campaign-finder-template>div').clone();
 
   $wrapper.css('background-image', 'url(' + this.image + ')');
 
+  // If this isn't a staff pick, remove the flag dif
   if (!this.staffPick) {
     $wrapper.find('.flag').remove();
   }
 
+  // If this is a featured campaign, let's make it big
   if (this.featured) {
     $wrapper.addClass('big');
   }
 
+  // Set the content
   $wrapper.find('p').text(this.description);
   $wrapper.find('h4').text(this.title);
   if (false) {
